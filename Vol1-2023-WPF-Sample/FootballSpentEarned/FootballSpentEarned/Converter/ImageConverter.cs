6baf8de8dbc78a859872386d853099bb7f430f9c
NanthiniMahalingam <nanthini.mahalingam@syncfusion.com>
2023-04-26 08:02:59 +0530

    I have added wpf sample

---
+++ Vol1-2023-WPF-Sample/FootballSpentEarned/FootballSpentEarned/Converter/ImageConverter.cs
@@ -0,0 +1,70 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FootballSpentEarned
+{
+    public class ImageConverter : IValueConverter
+    {
+        string Text { set; get; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Text = value as string;
+
+            if (Text.Contains(" ") || Text.Contains("�") || Text.Contains("ü") ||
+                Text.Contains("é") || Text.Contains("á") || Text.Contains("'"))
+            {
+                Text = Text.Replace(" ", "_").ToLower() + ".png";
+                Text = Text.Replace("&", "_");
+                Text = Text.Replace("�", "_");
+                Text = Text.Replace("ü", "_");
+                Text = Text.Replace("é", "_");
+                Text = Text.Replace("á", "_");
+                Text = Text.Replace("'", "_");
+                Text = Text.Replace("î", "_");
+                Text = Text.Replace("-", "_");
+            }
+            else if (!Text.Contains(".png"))
+            {
+                Text = Text.ToLower() + ".png";
+            }
+
+            if (Text.Contains("0") || Text.Contains("5") || Text.Contains("4"))
+            {
+                Text = Text.Replace("_05", "");
+                Text = Text.Replace("_04", "");
+            }
+
+            Text = "/FootballSpentEarned;component/ClubIcons/" + Text;
+            return Text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    public class Converter : IValueConverter
+    {
+        public int Count { get; set; } = 0;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                var data = value as LegendItem;
+                var series = data.Series as ScatterExt;
+                var str = "/FootballSpentEarned;component/ImageIcons/" + series.Source;
+                return str;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ Vol1-2023-WPF-Sample/FootballSpentEarned/FootballSpentEarned/ViewModel/FootballRevenue.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace FootballSpentEarned
+{
+    public class FootballRevenue
+    {
+        private List<FootballMarket> premierLeague;
+        public List<FootballMarket> PremierLeague
+        {
+            get { return premierLeague; }
+            set
+            {
+                premierLeague = value;
+            }
+        }
+
+        public List<FootballMarket> FlagCollection { get; set; }
+
+        private List<FootballMarket> liga;
+        public List<FootballMarket> Liga
+        {
+            get { return liga; }
+            set
+            {
+                liga = value;
+            }
+        }
+        private List<FootballMarket> ligue1;
+        public List<FootballMarket> Ligue1
+        {
+            get { return ligue1; }
+            set
+            {
+                ligue1 = value;
+            }
+        }
+        private List<FootballMarket> bundesliga;
+        public List<FootballMarket> Bundesliga
+        {
+            get { return bundesliga; }
+            set
+            {
+                bundesliga = value;
+            }
+        }
+        private List<FootballMarket> serieA;
+        public List<FootballMarket> SerieA
+        {
+            get { return serieA; }
+            set
+            {
+                serieA = value;
+            }
+
+        }
+
+        public FootballRevenue()
+        {
+            Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
+          
+            using (var stream = executingAssembly.GetManifestResourceStream("FootballSpentEarned.Resources.PremierLeague.json"))
+            using (TextReader textStream = new StreamReader(stream))
+            {
+                var data1 = textStream.ReadToEnd();
+                PremierLeague = JsonConvert.DeserializeObject<List<FootballMarket>>(data1);
+            }
+
+            using (var stream = executingAssembly.GetManifestResourceStream("FootballSpentEarned.Resources.Liga.json"))
+            using (TextReader textStream = new StreamReader(stream))
+            {
+                var data2 = textStream.ReadToEnd();
+                Liga = JsonConvert.DeserializeObject<List<FootballMarket>>(data2);
+            }
+
+            using (var stream = executingAssembly.GetManifestResourceStream("FootballSpentEarned.Resources.Ligue1.json"))
+            using (TextReader textStream = new StreamReader(stream))
+            {
+                var data3 = textStream.ReadToEnd();
+                Ligue1 = JsonConvert.DeserializeObject<List<FootballMarket>>(data3);
+            }
+
+            using (var stream = executingAssembly.GetManifestResourceStream("FootballSpentEarned.Resources.BundesLiga.json"))
+            using (TextReader textStream = new StreamReader(stream))
+            {
+                var data4 = textStream.ReadToEnd();
+                Bundesliga = JsonConvert.DeserializeObject<List<FootballMarket>>(data4);
+            }
+
+            using (var stream = executingAssembly.GetManifestResourceStream("FootballSpentEarned.Resources.SerieA.json"))
+            using (TextReader textStream = new StreamReader(stream))
+            {
+                var data5 = textStream.ReadToEnd();
+                SerieA = JsonConvert.DeserializeObject<List<FootballMarket>>(data5);
+            }
+        }
+    }
+}
