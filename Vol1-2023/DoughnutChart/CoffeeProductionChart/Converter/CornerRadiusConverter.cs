7528756704b7d1a04c2326bf1457b23b22d518a3
NanthiniMahalingam <nanthini.mahalingam@syncfusion.com>
2023-06-15 23:04:04 +0530

    To commit the changes.

--- Vol1-2023/DoughnutChart/CoffeeProductionChart/Converter/CornerRadiusConverter.cs
+++ Vol1-2023/DoughnutChart/CoffeeProductionChart/Converter/CornerRadiusConverter.cs
@@ -19,4 +19,17 @@ namespace CoffeeProductionChart
             return value;
         }
     }
+
+    public class DataLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
